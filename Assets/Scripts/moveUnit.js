d19c1fc49fc543e06de7f07db4c0d9d00e90b9cc
Jemmy Pavlic <pinnaclepavlic@gmail.com>
2017-03-16 21:55:51 -0400

    Dummy Unit
    Click to move a dummy unit around the terrain

---
+++ Assets/Scripts/moveUnit.js
@@ -0,0 +1,17 @@
+﻿var smoothMovement : float;
+private var targetPosition : Vector3;
+
+function FixedUpdate () {
+    if(Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKey(KeyCode.Mouse0)) {
+	    var playerPlane = new Plane(Vector3.up, transform.position);
+	    var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+	    var hitDist = 0.0;
+	        if(playerPlane.Raycast (ray, hitDist)) {
+	            var targetPoint = ray.GetPoint(hitDist);
+	            targetPosition = ray.GetPoint(hitDist);
+	            var targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+	            transform.rotation = targetRotation;
+	            }
+	    	}
+	transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime * smoothMovement);
+}
