123b48987b5e2529b4a90bea123ada14ad9391dd
Wayne Chen <chen.wayne@outlook.com>
2017-03-20 19:02:51 -0700

    adding new branch for camera work

---
+++ Assets/Scripts/mainCamera.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var distance : float = 60;
+var sensitivityDistance : float = 50;
+var damping : float = 5;
+
+var minFOV : float = 30;
+var maxFOV : float = 60;
+
+function Start ()
+{
+    distance = GetComponent.<Camera>().fieldOfView;
+}
+
+function Update ()
+{
+    distance -= Input.GetAxis("Mouse ScrollWheel") * sensitivityDistance;
+    distance = Mathf.Clamp(distance, minFOV, maxFOV);
+    GetComponent.<Camera>().fieldOfView = Mathf.Lerp(GetComponent.<Camera>().fieldOfView, distance, Time.deltaTime * damping);
+}
